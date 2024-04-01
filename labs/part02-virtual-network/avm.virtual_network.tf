45497e83558de7ec031e2ddc0e353f430ba94913
alenat10 <alex.ignatenko@gmail.com>
2024-04-01 11:32:34 -0400

    Update avm.virtual_network.tf
    vnet_ prefix is removed for :location" and "name". Othervise Terraform fails with "parameter is not expected here"

--- labs/part02-virtual-network/avm.virtual_network.tf
+++ labs/part02-virtual-network/avm.virtual_network.tf
@@ -4,6 +4,6 @@ module "virtual_network" {
   resource_group_name           = azurerm_resource_group.this.name
   subnets                       = local.subnets
   virtual_network_address_space = [local.virtual_network_address_space]
-  vnet_location                 = var.location
-  vnet_name                     = local.virtual_network_name
+  location                      = var.location
+  name                          = local.virtual_network_name
 }
