14b23bb2618023b6576201c9976f9bbde9827297
Jared Holgate <jaredholgate@microsoft.com>
2024-05-29 16:50:36 +0100

    Update bastion to latest version

--- labs/part05-virtual-machine/avm.bastion.tf
+++ labs/part05-virtual-machine/avm.bastion.tf
@@ -1,26 +1,21 @@
 module "bastion" {
   source  = "Azure/avm-res-network-bastionhost/azurerm"
-  version = "~> 0.1"
+  version = "~> 0.2"
 
-  resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = module.virtual_network.name
+  name                   = local.bastion_host_name
+  location               = azurerm_resource_group.this.location
+  resource_group_name    = azurerm_resource_group.this.name
+  copy_paste_enabled     = true
+  file_copy_enabled      = false
+  sku                    = "Standard"
+  ip_connect_enabled     = true
+  scale_units            = 2
+  shareable_link_enabled = true
+  tunneling_enabled      = true
 
-  bastion_host = {
-    name                   = local.bastion_host_name
-    location               = azurerm_resource_group.this.location
-    resource_group_name    = azurerm_resource_group.this.name
-    copy_paste_enabled     = true
-    file_copy_enabled      = false
-    sku                    = "Standard"
-    ip_connect_enabled     = true
-    scale_units            = 2
-    shareable_link_enabled = true
-    tunneling_enabled      = true
-
-    ip_configuration = {
-      name                 = "ipconfig"
-      subnet_id            = module.virtual_network.subnets["AzureBastionSubnet"].resource_id
-      public_ip_address_id = module.bastion_public_ip.public_ip_id
-    }
+  ip_configuration = {
+    name                 = "ipconfig"
+    subnet_id            = module.virtual_network.subnets["AzureBastionSubnet"].resource_id
+    public_ip_address_id = module.bastion_public_ip.public_ip_id
   }
 }
