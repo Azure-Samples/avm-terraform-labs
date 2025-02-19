7066cbc93c661de5962aa3691d8303b9e61c7fb8
Jared Holgate <jaredholgate@microsoft.com>
2025-02-19 18:24:01 +0000

    Fix bugs

--- labs/part05-virtual-machine/locals.tf
+++ labs/part05-virtual-machine/locals.tf
@@ -8,8 +8,8 @@ locals {
   network_security_group_name         = "nsg-demo-${local.unique_postfix}"
   nat_gateway_name                    = "natgw-demo-${local.unique_postfix}"
   nat_public_ip_name                  = "natpip-demo-${local.unique_postfix}"
-  storage_account_name                = replace("stdemo${local.unique_postfix}", "-", "")
   key_vault_name                      = "kv-demo-${format("%.16s", local.unique_postfix)}"
+  storage_account_name                = replace("stdemo${local.unique_postfix}", "-", "")
   user_assigned_managed_identity_name = "uami-demo-${local.unique_postfix}"
   virtual_machine_name                = "vm-demo-${local.unique_postfix}"
   network_interface_name              = "nic-demo-${local.unique_postfix}"
